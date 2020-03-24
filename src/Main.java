4aae519d892f020aa98a1d60b91c4cd901f3dc5c
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-03-24 17:13:15 -0300

    dev PriorityQueue

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalTime;
 import java.util.List;
 
 public class Main {
-	
-	public static final String DATABASE_FILE = "test.txt";
-	
+    public static final String DATABASE_FILE = "test.txt";
+    private static final LocalTime START = LocalTime.of(8, 00);
+    private static final LocalTime END = LocalTime.of(17, 00);
+
     public static void main(String[] args) throws IOException {
 
-    	File databaseFile = new File(DATABASE_FILE);
+        File databaseFile = new File(DATABASE_FILE);
 
-    	List<Client> database = FileConverter.getClientData(databaseFile);
-    	System.out.println(database);
-    	
+        List<Client> database = FileConverter.getClientData(databaseFile);
+
+        PriorityQueue priorityQueue = new PriorityQueue();
+
+        int finalized = priorityQueue.start(database, START, END);
+        System.out.println(priorityQueue.getReturnTime());
+        System.out.println(priorityQueue.getResponseTime());
+
 //        for (int i = 0; i < 500; i++) {
 //            System.out.println(PasswordGenerator.genereteNextPassword(Service.PAGAR_BOLETO));
 //        }
