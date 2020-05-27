ae793e650ba90667c93db21bb634f60c4b9e7d3c
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-05-27 19:00:38 -0300

    simple priority getting next
    moving getNext method to inside of the class

--- src/ClientSorter.java
+++ src/ClientSorter.java
@@ -93,72 +93,77 @@ public class ClientSorter {
 	}
 
 	public static void sortByPriorityFifo(List<Client> list, LocalTime aux) {
-		Collections.sort(list, new Comparator<Client>() {
 
-			LocalTime actual = aux;
-			List<String> ended = new ArrayList<String>();
-			int index = 1;
+		list.sort(Comparator.comparing(Client::getPriority).reversed());
+		list.sort(Comparator.comparing(Client::getArrivalTime));
+		System.out.println("list = " + list);
 
-			public void increase(Client c1, Client c2, int value) {
-				if(list.indexOf(c1) == index || list.indexOf(c1) == index) {
-					index++;
-				if (value < 0 && !ended.contains(c1.getCode())) {
-					actual = actual.plusHours(c1.getEstimatedTime().getHour());
-					actual = actual.plusMinutes(c1.getEstimatedTime().getMinute());
-					ended.add(c1.getCode());
-				} else if (!ended.contains(c2.getCode())) {
-					actual = actual.plusHours(c2.getEstimatedTime().getHour());
-					actual = actual.plusMinutes(c2.getEstimatedTime().getMinute());
-					ended.add(c2.getCode());
-				}
-				}
-				System.out.println("\nACTUAL = " + actual);
-				
-			}
-
-			@Override
-			public int compare(Client c1, Client c2) {
-				 System.out.println("\n\n============\n\nValores: c1 = " + c1 + " \n\nc2 = " +
-				 c2 );
-				LocalTime first = c1.getArrivalTime().compareTo(c2.getArrivalTime()) > 0 ? c2.getArrivalTime()
-						: c1.getArrivalTime();
-				if (actual.compareTo(first) < 0) {
-					actual = first;
-				}
-
-				if (c2.getPriority() == c1.getPriority()) {
-					System.out.println("caso 1");
-					int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
-					increase(c1, c2, chosen);
-					return chosen;
-				} else if (c2.getArrivalTime().equals(c1.getArrivalTime())) {
-					System.out.println("caso 2");
-					int chosen = c2.getPriority() - c1.getPriority();
-					increase(c2, c1, -1 * chosen);
-					return chosen;
-				} else if (actual.compareTo(c1.getArrivalTime()) > 0) {
-					if (actual.compareTo(c2.getArrivalTime()) > 0) {
-						System.out.println("caso 3");
-						int chosen = c2.getPriority() - c1.getPriority();
-						increase(c2,  c1, -1 * chosen);
-						return chosen;
-					} else {
-						System.out.println("caso 4");
-						int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
-						increase(c1, c2, chosen);
-						return chosen;
-					}
-
-				} else {
-					System.out.println("caso 5");
-					int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
-					increase(c1, c2, chosen);
-					return chosen;
-				}
-	
-			}
-
-		});
+//		Collections.sort(list, new Comparator<Client>() {
+//
+//			LocalTime actual = aux;
+//			List<String> ended = new ArrayList<String>();
+//			int index = 1;
+//
+//			public void increase(Client c1, Client c2, int value) {
+//				if(list.indexOf(c1) == index || list.indexOf(c1) == index) {
+//					index++;
+//				if (value < 0 && !ended.contains(c1.getCode())) {
+//					actual = actual.plusHours(c1.getEstimatedTime().getHour());
+//					actual = actual.plusMinutes(c1.getEstimatedTime().getMinute());
+//					ended.add(c1.getCode());
+//				} else if (!ended.contains(c2.getCode())) {
+//					actual = actual.plusHours(c2.getEstimatedTime().getHour());
+//					actual = actual.plusMinutes(c2.getEstimatedTime().getMinute());
+//					ended.add(c2.getCode());
+//				}
+//				}
+//				System.out.println("\nACTUAL = " + actual);
+//
+//			}
+//
+//			@Override
+//			public int compare(Client c1, Client c2) {
+//				 System.out.println("\n\n============\n\nValores: c1 = " + c1 + " \n\nc2 = " +
+//				 c2 );
+//				LocalTime first = c1.getArrivalTime().compareTo(c2.getArrivalTime()) > 0 ? c2.getArrivalTime()
+//						: c1.getArrivalTime();
+//				if (actual.compareTo(first) < 0) {
+//					actual = first;
+//				}
+//
+//				if (c2.getPriority() == c1.getPriority()) {
+//					System.out.println("caso 1");
+//					int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
+//					increase(c1, c2, chosen);
+//					return chosen;
+//				} else if (c2.getArrivalTime().equals(c1.getArrivalTime())) {
+//					System.out.println("caso 2");
+//					int chosen = c2.getPriority() - c1.getPriority();
+//					increase(c2, c1, -1 * chosen);
+//					return chosen;
+//				} else if (actual.compareTo(c1.getArrivalTime()) > 0) {
+//					if (actual.compareTo(c2.getArrivalTime()) > 0) {
+//						System.out.println("caso 3");
+//						int chosen = c2.getPriority() - c1.getPriority();
+//						increase(c2,  c1, -1 * chosen);
+//						return chosen;
+//					} else {
+//						System.out.println("caso 4");
+//						int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
+//						increase(c1, c2, chosen);
+//						return chosen;
+//					}
+//
+//				} else {
+//					System.out.println("caso 5");
+//					int chosen = c1.getArrivalTime().compareTo(c2.getArrivalTime());
+//					increase(c1, c2, chosen);
+//					return chosen;
+//				}
+//
+//			}
+//
+//		});
 	}
 
 }
--- src/SimplePriority.java
+++ src/SimplePriority.java
@@ -2,8 +2,8 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
-import static java.time.temporal.ChronoUnit.MINUTES;
-import static java.time.temporal.ChronoUnit.HOURS;;
+
+;
 
 public class SimplePriority implements Method {
     private List<Integer> responseTimes;
@@ -13,63 +13,80 @@ public class SimplePriority implements Method {
         responseTimes = new ArrayList<>();
         returnTimes = new ArrayList<>();
     }
-    
+
     @Override
     public int start(List<Client> list, LocalTime dayStart, LocalTime dayEnd) {
         int clientsFinalized = 0;
+        int initSize = list.size();
         LocalTime actual = dayStart;
-        
+
         ClientSorter.sortByArrive(list);
 
-        ClientSorter.sortByPriorityFifo(list, dayStart);
-        System.out.println(list + "\n\n\n");
-        
-        
-        for (Client client : list) {
-        	
-        	LocalTime wait = LocalTime.of(0,0);
-        	
-        	if(client.getArrivalTime().compareTo(actual) >= 0) {
-        		actual = client.getArrivalTime();
-        	} else {
-        		wait = actual.minusHours(client.getArrivalTime().getHour()).minusMinutes(client.getArrivalTime().getMinute());
-        	}
-        	        	
+//        ClientSorter.sortByPriorityFifo(list, dayStart);
+//        System.out.println(list + "\n\n\n");
+
+
+        for (int i = 0; i < initSize; i++) {
+
+            Client client = getNextCLientOf(list, actual);
+            System.out.println("\nclient = " + client);
+
+            LocalTime wait = LocalTime.of(0, 0);
+
+            if (client.getArrivalTime().compareTo(actual) >= 0) {
+                actual = client.getArrivalTime();
+            } else {
+                wait = actual.minusHours(client.getArrivalTime().getHour()).minusMinutes(client.getArrivalTime().getMinute());
+            }
+
             LocalTime startedAt = actual;
             LocalTime finalizeAt = startedAt.plusHours(client.getEstimatedTime().getHour()).plusMinutes(client.getEstimatedTime().getMinute());
 
             if (finalizeAt.isBefore(dayEnd)) {
-              System.out.println("Started at: " + startedAt + "\t|Should start at: " + client.getArrivalTime() + "\t\t|Prioridade: " + client.getPriority() + "\t|\tFinalized at: " + finalizeAt + "\t|\tEstimate: " + client.getEstimatedTime());
+                System.out.println("Started at: " + startedAt + "\t|Should start at: " + client.getArrivalTime() + "\t\t|Prioridade: " + client.getPriority() + "\t|\tFinalized at: " + finalizeAt + "\t|\tEstimate: " + client.getEstimatedTime());
                 actual = finalizeAt;
                 clientsFinalized++;
-               //System.out.println("startedAt.getMinute() = " + ((startedAt.getMinute() - dayStart.getMinute()) + (startedAt.getHour() - dayStart.getHour())*60  ));
+                //System.out.println("startedAt.getMinute() = " + ((startedAt.getMinute() - dayStart.getMinute()) + (startedAt.getHour() - dayStart.getHour())*60  ));
                 responseTimes.add(
                         (
                                 (
                                         wait.getMinute()
                                 ) + (
-                                		wait.getHour()
+                                        wait.getHour()
                                 ) * 60
                         )
                 );
                 returnTimes.add(
                         (
-                        		(
+                                (
                                         (finalizeAt.getMinute() - startedAt.getMinute()) + wait.getMinute()
                                 ) + (
-                                		(finalizeAt.getHour() - startedAt.getHour()) + wait.getHour()
+                                        (finalizeAt.getHour() - startedAt.getHour()) + wait.getHour()
                                 ) * 60
                         )
                 );
             }
-                    }
+        }
 
         return clientsFinalized;
     }
 
+    private Client getNextCLientOf(List<Client> list, LocalTime actual) {
+        Client returnClient = list.get(0);
+        for (Client client : list) {
+            if (client.getPriority() > returnClient.getPriority()) {
+                if (client.getArrivalTime().compareTo(actual) <= 0||client.getArrivalTime().compareTo(returnClient.getArrivalTime()) <= 0) {
+                    returnClient = client;
+                }
+            }
+        }
+        list.remove(returnClient);
+        return returnClient;
+    }
+
     @Override
     public double getResponseTime() {
-    	//System.out.println(responseTimes);
+        //System.out.println(responseTimes);
         OptionalDouble average = responseTimes.stream().mapToInt(Integer::valueOf).average();
         if (average.isPresent()) return average.getAsDouble();
         return 0;
